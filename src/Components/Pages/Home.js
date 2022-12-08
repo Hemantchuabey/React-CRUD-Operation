c841f6bde20db2e1f8d9aa6f9abb39beb6a0ad1f
Hemant <hementchaubey@gmail.com>
2022-12-08 08:25:16 +0530

    Added JSON server and it is working on localhost 3003 port.With help of axios we got the information from JSON server and using table view all the users are visible on the home page

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    loadUsers();
+  }, []);
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:3003/users");
+    setUsers(result.data);
+  };
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>UserName</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => {
+            return (
+              <tr key={index}>
+                <th className="userTh">{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
+                  <NavLink>
+                    <button className="view">View</button>
+                  </NavLink>
+                  <NavLink>
+                    <button className="edit">Edit</button>
+                  </NavLink>
+                  <NavLink>
+                    <button className="delete">Delete</button>
+                  </NavLink>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Home;
